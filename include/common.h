8fb295cff24396b3f556a6d8891e615bebd01397
MitzaaA14 <mihaip781@gmail.com>
2025-01-13 18:50:48 +0200

    Update V11

--- include/common.h
+++ include/common.h
@@ -4,29 +4,29 @@
 #include <string>
 
 class Nume {
-    private:
-        int id;
-        std::string nume;
-    public:
-        Nume() {}
-        Nume(const std::string& nume) : id(0), nume(nume) {}
+private:
+    int id;
+    std::string nume;
+public:
+    Nume() {}
+    Nume(const std::string& nume) : id(0), nume(nume) {}
 
-        virtual ~Nume() {}
+    virtual ~Nume() {}
 
-        const std::string& getNume() const { return nume; }
+    const std::string& getNume() const { return nume; }
 
-        void setId(const int setid) { this->id = setid; }
-        int getId() const { return id; }
+    void setId(const int setid) { this->id = setid; }
+    int getId() const { return id; }
 
-        virtual void print() = 0;
+    virtual void print() = 0;
 
-        bool operator<(const Nume& altul) const {
-            return getId() < altul.getId();
-        }
+    bool operator<(const Nume& altul) const {
+        return getId() < altul.getId();
+    }
 
-        bool operator==(const Nume& altul) const {
-            return getId() == altul.getId();
-        }        
+    bool operator==(const Nume& altul) const {
+        return getId() == altul.getId();
+    }
 };
 
 #endif
